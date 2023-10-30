7d3d87c04e20539aa400cc184303a1a786c9066c
OnlyDuy <vuquangduy002@gmail.com>
2023-10-30 21:45:15 +0700

    Home Page

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -47,7 +47,14 @@ dependencies {
     implementation("androidx.navigation:navigation-ui:2.6.0")
     implementation("com.google.firebase:firebase-auth:22.2.0")
     implementation("com.google.firebase:firebase-database:20.3.0")
+    implementation("com.google.firebase:firebase-firestore:24.9.1")
     testImplementation("junit:junit:4.13.2")
     androidTestImplementation("androidx.test.ext:junit:1.1.5")
     androidTestImplementation("androidx.test.espresso:espresso-core:3.5.1")
+
+    // Rounded ImageView
+    implementation("com.makeramen:roundedimageview:2.3.0")
+
+    // Glide
+    implementation("com.github.bumptech.glide:glide:4.16.0")
 }
