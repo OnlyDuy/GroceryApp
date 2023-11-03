808a78c87f9abaca6da99d60c61542230719e924
OnlyDuy <vuquangduy002@gmail.com>
2023-11-03 14:11:30 +0700

    ViewAll on PopularAdapter

---
+++ app/src/main/java/com/example/groceryapp/activities/ViewAllActivity.java
@@ -0,0 +1,80 @@
+package com.example.groceryapp.activities;
+
+import android.os.Bundle;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
+
+import com.example.groceryapp.R;
+import com.example.groceryapp.adapters.ViewAllAdapter;
+import com.example.groceryapp.models.ViewAllModel;
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.Firebase;
+import com.google.firebase.firestore.DocumentChange;
+import com.google.firebase.firestore.DocumentSnapshot;
+import com.google.firebase.firestore.FirebaseFirestore;
+import com.google.firebase.firestore.QuerySnapshot;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class ViewAllActivity extends AppCompatActivity {
+
+    FirebaseFirestore firestore;
+    RecyclerView recyclerView;
+    ViewAllAdapter viewAllAdapter;
+    List<ViewAllModel> viewAllModelList;
+    Toolbar toolbar;
+
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_view_all);
+
+        toolbar = findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+
+        firestore = FirebaseFirestore.getInstance();
+        String type = getIntent().getStringExtra("type");
+        recyclerView = findViewById(R.id.view_all_rec);
+        recyclerView.setLayoutManager(new LinearLayoutManager(this));
+
+        viewAllModelList = new ArrayList<>();
+        viewAllAdapter = new ViewAllAdapter(this,viewAllModelList);
+        recyclerView.setAdapter(viewAllAdapter);
+
+//        GETTING FRUITS
+        if (type != null && type.equalsIgnoreCase("fruit")){
+            firestore.collection("AllProducts").whereEqualTo("type", "fruit").get().addOnCompleteListener(new OnCompleteListener<QuerySnapshot>() {
+                @Override
+                public void onComplete(@NonNull Task<QuerySnapshot> task) {
+                    for (DocumentSnapshot documentSnapshot:task.getResult().getDocuments()){
+                        ViewAllModel viewAllModel = documentSnapshot.toObject(ViewAllModel.class);
+                        viewAllModelList.add(viewAllModel);
+                        viewAllAdapter.notifyDataSetChanged();
+                    }
+                }
+            });
+        }
+
+        //        GETTING VEGETABLES
+        if (type != null && type.equalsIgnoreCase("vegetable")){
+            firestore.collection("AllProducts").whereEqualTo("type", "vegetable").get().addOnCompleteListener(new OnCompleteListener<QuerySnapshot>() {
+                @Override
+                public void onComplete(@NonNull Task<QuerySnapshot> task) {
+                    for (DocumentSnapshot documentSnapshot:task.getResult().getDocuments()){
+                        ViewAllModel viewAllModel = documentSnapshot.toObject(ViewAllModel.class);
+                        viewAllModelList.add(viewAllModel);
+                        viewAllAdapter.notifyDataSetChanged();
+                    }
+                }
+            });
+        }
+    }
+}
--- app/src/main/java/com/example/groceryapp/adapters/PopularAdapters.java
+++ app/src/main/java/com/example/groceryapp/adapters/PopularAdapters.java
@@ -1,6 +1,7 @@
 package com.example.groceryapp.adapters;
 
 import android.content.Context;
+import android.content.Intent;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -12,6 +13,7 @@ import androidx.recyclerview.widget.RecyclerView;
 
 import com.bumptech.glide.Glide;
 import com.example.groceryapp.R;
+import com.example.groceryapp.activities.ViewAllActivity;
 import com.example.groceryapp.models.PopularModel;
 
 import java.util.List;
@@ -25,22 +27,6 @@ public class PopularAdapters extends RecyclerView.Adapter<PopularAdapters.ViewHo
         this.context = context;
         this.popularModelList = popularModelList;
     }
-
-    public class ViewHolder extends RecyclerView.ViewHolder {
-        ImageView popImg;
-        TextView name, description, rating, discount, type, img_url;
-        public ViewHolder(@NonNull View itemView) {
-            super(itemView);
-
-            popImg = itemView.findViewById(R.id.pop_img);
-            name = itemView.findViewById(R.id.pop_name);
-            description = itemView.findViewById(R.id.pop_des);
-            discount = itemView.findViewById(R.id.pop_discount);
-            rating = itemView.findViewById(R.id.pop_rating);;
-
-        }
-    }
-
     @NonNull
     @Override
     public ViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
@@ -48,16 +34,38 @@ public class PopularAdapters extends RecyclerView.Adapter<PopularAdapters.ViewHo
     }
 
     @Override
-    public void onBindViewHolder(@NonNull PopularAdapters.ViewHolder holder, int position) {
+    public void onBindViewHolder(@NonNull ViewHolder holder, int position) {
         Glide.with(context).load(popularModelList.get(position).getImg_url()).into(holder.popImg);
         holder.name.setText(popularModelList.get(position).getName());
         holder.rating.setText(popularModelList.get(position).getRating());
         holder.description.setText(popularModelList.get(position).getDescription());
         holder.discount.setText(popularModelList.get(position).getDiscount());
+
+        holder.itemView.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(context, ViewAllActivity.class);
+                intent.putExtra("type", popularModelList.get(holder.getAdapterPosition()).getType());
+                context.startActivity(intent);
+            }
+        });
     }
 
     @Override
     public int getItemCount() {
         return popularModelList.size();
+    }
+
+    public class ViewHolder extends RecyclerView.ViewHolder {
+        ImageView popImg;
+        TextView name, description, rating, discount;
+        public ViewHolder(@NonNull View itemView) {
+            super(itemView);
+            popImg = itemView.findViewById(R.id.pop_img);
+            name = itemView.findViewById(R.id.pop_name);
+            description = itemView.findViewById(R.id.pop_des);
+            discount = itemView.findViewById(R.id.pop_discount);
+            rating = itemView.findViewById(R.id.pop_rating);
+        }
     }
 }
