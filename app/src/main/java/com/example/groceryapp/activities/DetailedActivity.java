b43451a1d7775643cf4ba6bb03120da235d399e8
OnlyDuy <vuquangduy002@gmail.com>
2023-11-11 10:15:26 +0700

    Add To Cart

--- app/src/main/java/com/example/groceryapp/activities/DetailedActivity.java
+++ app/src/main/java/com/example/groceryapp/activities/DetailedActivity.java
@@ -1,5 +1,6 @@
 package com.example.groceryapp.activities;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.Toolbar;
 
@@ -8,10 +9,21 @@ import android.view.View;
 import android.widget.Button;
 import android.widget.ImageView;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import com.bumptech.glide.Glide;
 import com.example.groceryapp.R;
 import com.example.groceryapp.models.ViewAllModel;
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.Firebase;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.firestore.DocumentReference;
+import com.google.firebase.firestore.FirebaseFirestore;
+
+import java.text.SimpleDateFormat;
+import java.util.Calendar;
+import java.util.HashMap;
 
 public class DetailedActivity extends AppCompatActivity {
 
@@ -24,6 +36,9 @@ public class DetailedActivity extends AppCompatActivity {
     ImageView addItem, removeItem;
     Toolbar toolbar;
 
+    FirebaseFirestore firestore;
+    FirebaseAuth auth;
+
     ViewAllModel viewAllModel = null;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -33,6 +48,9 @@ public class DetailedActivity extends AppCompatActivity {
         toolbar = findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
         getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+
+        firestore = FirebaseFirestore.getInstance();
+        auth = FirebaseAuth.getInstance();
 
         final Object object = getIntent().getSerializableExtra("detail");
         if (object instanceof ViewAllModel) {
@@ -65,6 +83,12 @@ public class DetailedActivity extends AppCompatActivity {
         }
 
         addToCart = findViewById(R.id.add_to_cart);
+        addToCart.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                addedToCart();
+            }
+        });
         addItem.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
@@ -99,5 +123,34 @@ public class DetailedActivity extends AppCompatActivity {
                 }
             }
         });
+    }
+
+    private void addedToCart() {
+        String saveCurrentDate, saveCurrentTime;
+        Calendar calForDate = Calendar.getInstance();
+
+        SimpleDateFormat currentDate = new SimpleDateFormat("MM dd, yyyy");
+        saveCurrentDate = currentDate.format(calForDate.getTime());
+
+        SimpleDateFormat currentTime = new SimpleDateFormat("HH:mm:ss a");
+        saveCurrentTime = currentTime.format(calForDate.getTime());
+
+        final HashMap<String,Object> cartMap = new HashMap<>();
+
+        cartMap.put("productName", viewAllModel.getName());
+        cartMap.put("productPrice", price.getText().toString());
+        cartMap.put("currentDate", saveCurrentDate);
+        cartMap.put("currentTime", saveCurrentTime);
+        cartMap.put("totalQuantity", totalQuantity);
+        cartMap.put("totalPrice", totalPrice);
+
+        firestore.collection("AddToCart").document(auth.getCurrentUser().getUid())
+                .collection("CurrentUser").add(cartMap).addOnCompleteListener(new OnCompleteListener<DocumentReference>() {
+                    @Override
+                    public void onComplete(@NonNull Task<DocumentReference> task) {
+                        Toast.makeText(DetailedActivity.this, "Added To A Cart", Toast.LENGTH_SHORT).show();
+                        finish();
+                    }
+                });
     }
 }
