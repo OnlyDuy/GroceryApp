4afe8c81fe9c081d970417fefae01d8bb5100994
Vu Quang Duy <95005474+OnlyDuy@users.noreply.github.com>
2023-11-26 17:15:27 +0700

    View My Cart

--- app/src/main/java/com/example/groceryapp/MyCartsFragment.java
+++ app/src/main/java/com/example/groceryapp/MyCartsFragment.java
@@ -1,15 +1,47 @@
 package com.example.groceryapp;
 
+import android.content.BroadcastReceiver;
+import android.content.Context;
+import android.content.Intent;
+import android.content.IntentFilter;
+import android.media.tv.BroadcastInfoRequest;
 import android.os.Bundle;
 
+import androidx.annotation.NonNull;
 import androidx.fragment.app.Fragment;
+import androidx.localbroadcastmanager.content.LocalBroadcastManager;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.TextView;
+
+import com.example.groceryapp.adapters.MyCartAdapter;
+import com.example.groceryapp.models.MyCartModel;
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.firestore.DocumentSnapshot;
+import com.google.firebase.firestore.FirebaseFirestore;
+import com.google.firebase.firestore.QuerySnapshot;
+
+import java.util.ArrayList;
+import java.util.List;
 
 
 public class MyCartsFragment extends Fragment {
+
+    FirebaseFirestore db;
+    FirebaseAuth auth;
+
+    RecyclerView recyclerView;
+    MyCartAdapter cartAdapter;
+    List<MyCartModel> cartModeList;
+
+    TextView overTotalAmount;
+
     public MyCartsFragment() {
         // Required empty public constructor
     }
@@ -17,6 +49,43 @@ public class MyCartsFragment extends Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_my_carts, container, false);
+        View root = inflater.inflate(R.layout.fragment_my_carts, container, false);
+
+        db = FirebaseFirestore.getInstance();
+        auth = FirebaseAuth.getInstance();
+        recyclerView = root.findViewById(R.id.recyclerview);
+        recyclerView.setLayoutManager(new LinearLayoutManager(getActivity()));
+
+        overTotalAmount = root.findViewById(R.id.total_view_price);
+        LocalBroadcastManager.getInstance(getActivity())
+                .registerReceiver(mMessengeReceiver, new IntentFilter("MyTotalAmount"));
+
+        cartModeList = new ArrayList<>();
+        cartAdapter = new MyCartAdapter(getActivity(), cartModeList);
+        recyclerView.setAdapter(cartAdapter);
+
+        db.collection("AddToCart").document(auth.getCurrentUser().getUid())
+                .collection("CurrentUser").get().addOnCompleteListener(new OnCompleteListener<QuerySnapshot>() {
+                    @Override
+                    public void onComplete(@NonNull Task<QuerySnapshot> task) {
+                        if (task.isSuccessful()) {
+                            for (DocumentSnapshot documentSnapshot : task.getResult().getDocuments()) {
+                                MyCartModel cartModel = documentSnapshot.toObject(MyCartModel.class);
+                                cartModeList.add(cartModel);
+                                cartAdapter.notifyDataSetChanged();
+                            }
+                        }
+                    }
+                });
+
+        return root;
     }
+
+    public BroadcastReceiver mMessengeReceiver = new BroadcastReceiver() {
+        @Override
+        public void onReceive(Context context, Intent intent) {
+            int totalBill = intent.getIntExtra("totalAmount", 0);
+            overTotalAmount.setText("Total Bill: " + totalBill + "$");
+        }
+    };
 }
